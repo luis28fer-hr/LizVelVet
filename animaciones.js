d3a9a033c08230710e48a3492bdfd5176da49221
Luis-Fernando-Hernandez-Reyes <116451159+luis28fer-hr@users.noreply.github.com>
2024-10-17 18:22:08 -0600

    Especialidades no responsive

--- animaciones.js
+++ animaciones.js
@@ -103,3 +103,39 @@ navLinks.forEach(link => {
         link.classList.add('link.active');
     });
 });
+
+
+
+
+const sliders = document.querySelectorAll('.container-especialidades');
+
+sliders.forEach(slider => {
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    slider.addEventListener('mousedown', (e) => {
+        isDown = true;
+        slider.classList.add('active');
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+    });
+
+    slider.addEventListener('mouseleave', () => {
+        isDown = false;
+        slider.classList.remove('active');
+    });
+
+    slider.addEventListener('mouseup', () => {
+        isDown = false;
+        slider.classList.remove('active');
+    });
+
+    slider.addEventListener('mousemove', (e) => {
+        if (!isDown) return; // Solo hacer algo si el mouse está presionado
+        e.preventDefault();
+        const x = e.pageX - slider.offsetLeft;
+        const walk = (x - startX) * 3; // La cantidad de desplazamiento
+        slider.scrollLeft = scrollLeft - walk;
+    });
+});
