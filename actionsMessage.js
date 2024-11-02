4002f8181dcf8996c9478d739178e13a4a2bb465
Luis-Fernando-Hernandez-Reyes <116451159+luis28fer-hr@users.noreply.github.com>
2024-11-02 11:48:39 -0600

    Mensajes personalizados Especialidades

--- actionsMessage.js
+++ actionsMessage.js
@@ -18,3 +18,20 @@ whatsappDiv.addEventListener('click', function () {
     // Abrimos la URL de WhatsApp en una nueva ventana o pestaña (_blank)
     window.open(whatsappURL, '_blank');
 });
+
+
+
+// JavaScript: Función para enviar el mensaje
+function enviarMensaje(servicio) {
+    // Número de teléfono de WhatsApp en formato internacional (sin +)
+    const phoneNumber = '+524482015506'; // Cambia este número por el tuyo
+    // Mensaje que quieres que se envíe automáticamente
+    const message = `¡Hola! 😊✨ Me encantaría agendar una cita para el servicio de *${servicio}* 💅💆‍♀️. Estoy muy interesada y quisiera saber todos los detalles: costo 💸, qué incluye 📋 y la disponibilidad de turnos 🕒. ¡Gracias por tu ayuda! 🌟`;
+
+    // Codificamos el mensaje para que sea parte de la URL
+    const encodedMessage = encodeURIComponent(message);
+
+    // Construimos la URL de WhatsApp con el número y el mensaje codificado
+    const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+    window.open(whatsappURL, '_blank');
+}
