12dca128c3f707984e8e46d26bf7dcb757fd4c9e
Luis-Fernando-Hernandez-Reyes <116451159+luis28fer-hr@users.noreply.github.com>
2024-12-05 22:15:24 -0600

    Actualizacion navideña opacity

--- navidad.js
+++ navidad.js
@@ -4,16 +4,16 @@ const createSnowflake = () => {
   snowflake.classList.add("snowflake");
   snowflake.textContent = "❄";
   snowflake.style.left = Math.random() * window.innerWidth + "px";
-  snowflake.style.animationDuration = Math.random() * 3 + 10 + "s"; // Caída más rápida o más lenta
-  snowflake.style.fontSize = Math.random() * 10 + 21 + "px"; // Tamaño de copos variado
-  snowflake.style.opacity = Math.random() * 0.8 + 0.4;
+  snowflake.style.animationDuration = Math.random() * 2 + 10 + "s"; // Caída más rápida o más lenta
+  snowflake.style.fontSize = Math.random() * 6 + 21 + "px"; // Tamaño de copos variado
+  snowflake.style.opacity = Math.random() * 0.75 + 0.25;
 
   snowContainer.appendChild(snowflake);
 
   // Eliminar el copo después de que haya salido de la pantalla
   setTimeout(() => {
     snowflake.remove();
-  }, 7500);
+  }, 8000);
 };
 
 // Crear copos de nieve continuamente
